71fb8402e23df4a1423c152a3fc8ac20ae14092a
Ch4uwa <mrtn.karlsson@gmail.com>
2019-02-08 15:02:16 +0100

    Removed distance sens

--- wifi.ino
+++ wifi.ino
@@ -1,38 +1,33 @@
 #include <SoftwareSerial.h>
 #include "WiFiEsp.h"
 #include "dht.h"
-#include "NewPing.h"
-#include "Servo.h"
+
 
 #define dht_apin A0
-#define TRIG_PIN 2
-#define ECHO_PIN 3
-#define MAX_DISTANCE 200
+
+
 
 // Create WiFi module object on GPIO pin 6 (RX) and 7 (TX)
 SoftwareSerial Serial1(6, 7);
 WiFiEspClient client;
 dht DHT;
 
-Servo servo_9;
-NewPing sonar(TRIG_PIN, ECHO_PIN, MAX_DISTANCE);
 
 // Declare and initialise global arrays for WiFi settings
-const char ssid[] = "AndroidAP";
-const char pass[] = "xxx";
+const char ssid[] = "MartinAP";
+const char pass[] = "qwertyui";
 const char server[] = "primat.se";
 const String hostname = "primat.se";
-const String uri = "/services/data/mamati@sti.se-mamati.json";
-const String senduri = "/services/sendform.aspx?xid=mamati&xmail=mamati@sti.se";
+const String uri = "/services/data/mamati@sti.se-mamati.csv";
+const String senduri = "/services/sendform.aspx?xdata=mamati@sti.se|mamati|";
 const int port = 80;
 
-String xid = "mamati";
-String xmail = "mamati@sti.se";
 
-String data1;
-String data2;
+String dataT;
+String dataH;
+
 String line;
-int distance = 100;
+
 // Declare and initialise variable for radio status
 int status = WL_IDLE_STATUS;
 
@@ -44,47 +39,22 @@ void setup()
     // Initialize serial
     Serial1.begin(9600);
 
-    servo_9.attach(9);
     //Initialize wifi and connect
     wifiInit();
-
-
 }
 
 void loop()
 {
 
     tempHumid();
-    
-    for (size_t pos = 0; pos <= 180; pos+=10)
-    {
-        servo_9.write(pos);
-    
-        distance = readDistance();
-        Serial.println("at pos: ");
-        Serial.print(pos);
-        Serial.print(" ");
-        Serial.print(distance);
-        Serial.println();
-        
-    }
-
-    delay(1000);
-
-    for(size_t pos = 180; pos > 0; pos--)
-    {
-        servo_9.write(pos);
-        delay(10);
-    }
-/*
-    float x = cos(pos * 3.1415 / 180) * distance;
-    float y = sin(pos * 3.1415 / 180) * distance;
-*/
     delay(1000);
     sendData();
+    
+    
+    //delay(500);
+    //sendData();
     delay(1000);
     readData();
-
 
     // if the server's disconnected, stop the client
     if (!client.connected())
@@ -97,8 +67,6 @@ void loop()
 
     delay(300000);
 }
-
-
 
 void wifiInit()
 {
@@ -145,11 +113,9 @@ int readDistance()
 void tempHumid()
 {
     DHT.read11(dht_apin);
+    dataT = "Temperature:" + String(DHT.temperature);
+    dataH = "Humidity:" + String(DHT.humidity);
 
-    data1 = "&Temperature=" + String(DHT.temperature);
-    data2 = "&Humidity=" + String(DHT.humidity);
-    Serial.println(data1);
-    Serial.println(data2);
     delay(5000); //Wait 5 seconds before accessing sensor again.
                  //Fastest should be once every two seconds.
 }
@@ -167,7 +133,6 @@ void readData()
         client.println("Host: " + hostname);
         client.println("Connection: close");
         client.println();
-        
     }
     /* if there are incoming bytes available
      * from the server, read them and print them */
@@ -184,19 +149,21 @@ void readData()
 
 void sendData()
 {
+
     Serial.println();
     Serial.println("Sending to server...");
     // if you get a connection, report back via serial
-
     if (client.connect(server, port))
     {
         Serial.println("Connected to server");
-
-        client.println("GET " + senduri + data1 + data2 + " HTTP/1.1");
+        delay(50);
+        client.println("GET " + senduri + dataT + dataH + " HTTP/1.1");
         client.println("Host: " + hostname);
         client.println("Connection: close");
         client.println();
     }
+    
+
     while (client.available() == 0)
     {
         delay(5);
